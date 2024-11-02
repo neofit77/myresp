ebccded8c7c0c9fed995b132f00f40817407e2f2
neofit77 <aleksandar.petrovicvr@gmail.com>
2024-11-02 15:59:24 +0100

    Novi commit

--- src/posete/models.py
+++ src/posete/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Gost(models.Model):
-    ime = models.TextField()
+    ime = models.TextField(blank=True)
     prezime = models.TextField()
     godina_rodjenja = models.IntegerField()
     datum_prijave = models.DateTimeField(auto_now_add=True)
